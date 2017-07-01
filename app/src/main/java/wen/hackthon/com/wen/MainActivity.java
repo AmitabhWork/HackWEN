c77f6d3f84d14daf04264810fb9e19a1dd1bbea0
sairamana7 <sairamana6@gmail.com>
2017-07-01 12:34:51 +0530

    Home Screen Added

--- app/src/main/java/wen/hackthon/com/wen/MainActivity.java
+++ app/src/main/java/wen/hackthon/com/wen/MainActivity.java
@@ -1,15 +1,41 @@
 package wen.hackthon.com.wen;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Intent homescareen = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(homescareen);
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
 
+                }
+                // ...
+            }
+        };
     }
 }
