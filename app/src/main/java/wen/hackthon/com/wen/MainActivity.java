37c1223fcf6ae5be41a0f262df24b9d57f215589
sairamana7 <sairamana6@gmail.com>
2017-07-01 12:46:57 +0530

    Home Screen Added                                                                                                                                     Fragment removed and code placed in activity

--- app/src/main/java/wen/hackthon/com/wen/MainActivity.java
+++ app/src/main/java/wen/hackthon/com/wen/MainActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppCompatButton signup, signin;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -19,7 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        signin = (AppCompatButton) findViewById(R.id.SignIn);
+        signup = (AppCompatButton) findViewById(R.id.SignupButton);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupintent = new Intent(getApplicationContext(), VerifyFragment.class);
+                startActivity(signupintent);
+            }
+        });
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
